1d0a9f99d5e0f13c70d5107f7b54c2d817ffe20a
FallOfHeaven <vincentc268@nycstudents.net>
2025-05-29 11:55:03 -0400

    Update V1.java

--- Moving/V1.java
+++ Moving/V1.java
@@ -1,1 +1,14 @@
-
+public class TestingFinch{
+	public static void main(String[] args) {
+		Finch a = new Finch();
+		
+		while(true) {
+			while(a.getDistance() < 15) {
+				System.out.println(a.getDistance());
+				a.setTurn("R", 10, 5);
+			}
+			a.setMove("F", 10, 10);
+			
+		}
+	}
+}
